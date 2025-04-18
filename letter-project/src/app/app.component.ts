67d85b1f65e9df5824e615abf84cb4dcd0c22152
Buraktalhaa <talha45879@gmail.com>
2025-04-18 08:59:11 +0300

    project completed

--- letter-project/src/app/app.component.ts
+++ letter-project/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'letter-project';
+  randomText = faker.lorem.sentence()
+  enteredText = ''
+
+
+  getValue(event: InputEvent) {
+    console.log(this.randomText.split(''))
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
+    this.enteredText = input.value
+  }
+
+  compare(letter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return letter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
