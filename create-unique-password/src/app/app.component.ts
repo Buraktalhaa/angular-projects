7ad720ff8ea146bde20b7755d93ee6570fd98c17
Buraktalhaa <talha45879@gmail.com>
2025-04-21 09:02:09 +0300

    some minor changes have been made in create-unique-password project

--- create-unique-password/src/app/app.component.ts
+++ create-unique-password/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +11,8 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'password';
   password = ''
-
   length: number = 0;
+
   includeLetters: boolean = false;
   includeNumbers: boolean = false;
   includeSymbols: boolean = false;
